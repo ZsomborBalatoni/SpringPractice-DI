c8c7c4eb63ff4a5865ad492187ba53b0fee5703f
ZsomborBalatoni <zsombor.balatoni@gmail.com>
2023-03-26 13:33:38 +0200

    Creating appcontext

--- src/main/java/g2z/springframework/SpringPracticeDI/SpringPracticeDiApplication.java
+++ src/main/java/g2z/springframework/SpringPracticeDI/SpringPracticeDiApplication.java
@@ -1,13 +1,21 @@
 package g2z.springframework.SpringPracticeDI;
 
+import g2z.springframework.SpringPracticeDI.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringPracticeDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringPracticeDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringPracticeDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
