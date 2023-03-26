e9ccf804f10bf9202bf8ebba3d6d080a787097ad
ZsomborBalatoni <zsombor.balatoni@gmail.com>
2023-03-26 13:21:23 +0200

    Uploading project

---
+++ src/main/java/g2z/springframework/SpringPracticeDI/SpringPracticeDiApplication.java
@@ -0,0 +1,13 @@
+package g2z.springframework.SpringPracticeDI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeDiApplication.class, args);
+	}
+
+}
